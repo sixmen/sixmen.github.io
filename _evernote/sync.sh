9e7b83e04715477893b68a48a410c88c2ddd5864
Sangmin Yoon <sangmin.yoon@croquis.com>
2014-02-27 12:30:41 +0900

    update Evernote sync script

--- _evernote/sync.sh
+++ _evernote/sync.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-#git pull origin master
-rm ../_posts/mylife/*.html
+git pull origin master
+
+rm ../_posts/*/*.html
 mkdir -p ../_posts/mylife
-mkdir -p ../assets/mylife
+mkdir -p ../assets/evernote
+
 coffee fetch.coffee
-#git add ../_posts/mylife
-#git commit -m 'posts from Evernote'
-#git push origin master
+
+git add ../_posts
+git commit -m 'update posts from Evernote'
+
+git push origin master
+
+cd ..
+jekyll build
+cd _site
+git commit -a -m 'update posts from Evernote'
+git push origin master
