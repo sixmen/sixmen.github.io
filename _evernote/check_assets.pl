cb761a8f52e8591362eed7c36eac0c217d2bad81
Sangmin Yoon <sangmin.yoon@croquis.com>
2014-06-12 15:22:53 +0900

    remove unused assets

---
+++ _evernote/check_assets.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+@assets = `ls ../assets/evernote`;
+chomp @assets;
+@posts = `ls ../_posts/evernote`;
+
+foreach $post (@posts) {
+  open INPUT, "<../_posts/evernote/$post";
+  $content = join('', <INPUT>);
+  close INPUT;
+  @matches = $content =~ /{{ ASSET_PATH }}\/evernote\/([a-z0-9-.]*)/g;
+  foreach $match (@matches) {
+    @assets = grep { $_ ne $match } @assets;
+  }
+}
+
+foreach $asset (@assets) {
+  `git rm -f ../assets/evernote/$asset`;
+}
